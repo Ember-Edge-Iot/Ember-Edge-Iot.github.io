a20934e393f2354cb8cf1041a6e11dd88bea7aed
Aro Barath Chandru B <emberedgeiot@gmail.com>
2023-11-05 16:21:25 +0530

    .
    .

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let countDownDate = new Date('Nov 21, 2023 00:00:00').getTime();
+let countDownDate = new Date('Nov 26, 2023 00:00:00').getTime();
 
 let x = setInterval(function () {
   let now = new Date().getTime();
